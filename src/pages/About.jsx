8649544347d597eed607a2cb78905f87ee2ddca5
Nelson Foltz <nelsfoltz@gmail.com>
2020-12-10 16:07:02 -0500

    add: about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React from 'react';
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Roof party pabst polaroid tote bag, thundercats beard kale chips salvia.
+        Cornhole bespoke activated charcoal organic everyday carry mlkshk
+        typewriter franzen artisan cardigan narwhal taxidermy vaporware. Hammock
+        air plant tbh kogi, cold-pressed taiyaki vaporware meditation mlkshk
+        pickled 90's. Listicle hoodie flexitarian food truck enamel pin selfies
+        succulents unicorn tacos leggings next level.
+      </p>
+    </section>
+  );
 }
