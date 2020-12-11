58412c2ba6cb1bd17cf7eee21d62305a82d0ebb1
Nelson Foltz <nelsfoltz@gmail.com>
2020-12-11 15:24:28 -0500

    add: useEffect setup in products context

--- src/context/products.js
+++ src/context/products.js
@@ -1,11 +1,17 @@
-import React, { createContext, useState } from 'react';
-
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
+import url from '../utils/URL';
 export const ProductContext = createContext();
 
 export default function ProductProvider({ children }) {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
   const [featured, setFeatured] = useState([]);
+
+  useEffect(() => {
+    axios.get(url).then(storeProducts => console.log(storeProducts));
+    return () => {};
+  }, []);
 
   return (
     <ProductContext.Provider value={{ loading, products, featured }}>
