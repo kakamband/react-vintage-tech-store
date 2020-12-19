e34223c7e9ab1f5aafc3c7a20443c2842aa7a491
Nelson Foltz <nelsfoltz@gmail.com>
2020-12-19 13:55:08 -0500

    add: empty cart component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,17 @@
 import React, { useContext } from 'react';
 import { CartContext } from '../context/cart';
+import EmptyCart from '../components/Cart/EmptyCart';
+import CartItem from '../components/Cart/CartItem';
+import { Link } from 'react-router-dom';
+// import { UserContext } from '../context/user';
 
 export default function Cart() {
+  let user = false;
   const { cart, total } = useContext(CartContext);
-  console.log({ cart, total });
+
+  if (cart.length === 0) {
+    return <EmptyCart />;
+  }
+
   return <h1>hello from cart page</h1>;
 }
