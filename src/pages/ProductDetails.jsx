c7a991ecb4aa031cc89b555afa9c874e8b977da7
Nelson Foltz <nelsfoltz@gmail.com>
2020-12-19 12:52:13 -0500

    add: single product

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import { ProductContext } from '../context/products';
+// import {CartContext} from '../context/cart';
+import { useHistory } from 'react-router-dom';
+import Loading from '../components/Loading';
 
 export default function ProductDetails() {
   const { id } = useParams();
+  const history = useHistory();
+  const { products } = useContext(ProductContext);
+  const product = products.find(item => item.id === parseInt(id));
 
-  return <h1>hello from product details page. product id is : {id}</h1>;
+  if (products.length === 0) {
+    return <Loading />;
+  } else {
+    const {
+      image: { url },
+      title,
+      price,
+      description
+    } = product;
+
+    return (
+      <section className="single-product">
+        <img src={url} alt={title} className="single-product-image" />
+        <article>
+          <h1>{title}</h1>
+          <h2>{price}</h2>
+          <p>{description}</p>
+          <button
+            className="btn btn-primary btn-block"
+            onClick={() => {
+              // add to cart
+              history.push('/cart');
+            }}
+          >
+            add to cart
+          </button>
+        </article>
+      </section>
+    );
+  }
 }
